3ddd5e5a37fb6043d1fd03f0a6e1ebe908609e0d
bdavisx <bdavisx@yahoo.com>
2016-11-16 06:05:20 -0600

    MyList and other code from beginning of chapter now working.

--- src/main/kotlin/ws/billdavis/fpik/chapter03/MyList.kt
+++ src/main/kotlin/ws/billdavis/fpik/chapter03/MyList.kt
@@ -2,10 +2,7 @@ package ws.billdavis.fpik.chapter03
 
 import io.kotlintest.KTestJUnitRunner
 import io.kotlintest.specs.FeatureSpec
-import io.kotlintest.specs.ShouldSpec
-import org.junit.Assert
 import org.junit.runner.RunWith
-import ws.billdavis.fpik.chapter02.uncurry
 
 sealed class MyList<out A>
 object Nil: MyList<Nothing>()
@@ -27,23 +24,54 @@ fun product(ds: MyList<Double>): Double = when(ds) {
 fun <A> myListOf(vararg values: A): MyList<A> {
     if(values.size == 0) return Nil;
 
-    tailrec fun loop(myList: MyList<A>, index: Int): MyList<A> {
-        if (index == 0) return myList
-        if (index == 1) return Cons(values.first(), Nil)
+    tailrec fun loop(list: MyList<A>, index: Int): MyList<A> {
+        if (index == 0) return list
 
-        return loop(Cons(values[index], myList), index-1)
+        return loop(Cons(values[index-1], list), index-1)
     }
 
-    return loop(Cons(values.first(), Nil), values.size-1)
+    return loop(Cons(values.last(), Nil), values.size-1)
 }
 
 @RunWith(KTestJUnitRunner::class)
 class MyListTests: FeatureSpec() {
     init {
-        feature("myListOf") {
+        feature("sum(MyList<A>)") {
+            scenario("Nil should return 0") {
+                sum(Nil) shouldBe 0
+            }
+
+            scenario("Correctly sum values") {
+                sum(myListOf(5)) shouldBe 5
+                sum(myListOf(1,5,7)) shouldBe 13
+            }
+        }
+        feature("product(MyList<A>)") {
+            scenario("Nil should return 0") {
+                product(Nil) shouldBe 1.0
+            }
+
+            scenario("Correctly sum values") {
+                product(myListOf(5.0)) shouldBe 5.0
+                product(myListOf(2.0, 5.0, 7.0)) shouldBe 70.0
+            }
+        }
+        feature("myListOf<A>()") {
             scenario("create an empty list if not passed anything") {
                 val test = myListOf<Int>()
                 (test == Nil) shouldBe true
+            }
+
+            scenario("handle a single entry list and return the correct value from it") {
+                val test: Cons<Int> = myListOf<Int>(1) as Cons<Int>
+                test.head shouldBe 1
+            }
+
+            scenario("handle a multiple entry list and return the correct values from it") {
+                val test: Cons<Int> = myListOf<Int>(2, 1) as Cons<Int>
+                test.head shouldBe 2
+                val next: Cons<Int> = test.tail as Cons<Int>
+                next.head shouldBe 1
             }
         }
     }
