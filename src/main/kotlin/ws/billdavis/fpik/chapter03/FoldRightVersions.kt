524ddb5fea1a250bf5bc6811e72e75de29801386
bill <bdavisx@yahoo.com>
2016-11-19 18:37:46 -0600

    Refined productFR, created length() function.

--- src/main/kotlin/ws/billdavis/fpik/chapter03/FoldRightVersions.kt
+++ src/main/kotlin/ws/billdavis/fpik/chapter03/FoldRightVersions.kt
@@ -9,15 +9,24 @@ fun <A,B> foldRight(list: MyList<A>, zero: B, f: (A,B) -> B): B = when(list) {
     is Cons -> f(list.head, foldRight(list.tail, zero, f))
 }
 
-fun productFR(ds: MyList<Double>): Double {
-    return foldRight<Double, Double>(ds, 1.0, {a, b -> a*b})
-}
+fun productFR(ds: MyList<Double>): Double = foldRight(ds, 1.0, {a, b -> a*b})
 
-
+fun <A> length(list: MyList<A>): Int = foldRight(list, 0, {a, b -> b+1})
 
 @RunWith(KTestJUnitRunner::class)
 class Chapter3_7Tests: FeatureSpec() {
     init {
+        feature("length MyList<A>") {
+            scenario("Nil should return 0") {
+                length(Nil) shouldBe 0
+            }
+            scenario("single element should return 1") {
+                length(myListOf(1)) shouldBe 1
+            }
+            scenario("multi element list should return correct length") {
+                length(myListOf(1,2,3,4)) shouldBe 4
+            }
+        }
         feature("productFR MyList<A>") {
             scenario("Nil should return 1.0") {
                 productFR(Nil) shouldBe (1.0 plusOrMinus 0.0)
