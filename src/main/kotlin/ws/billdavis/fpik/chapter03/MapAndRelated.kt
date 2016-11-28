b0d10af0b67ac3c9d60eaafd91af17d47a9880b8
bdavisx <bdavisx@yahoo.com>
2016-11-27 20:17:12 -0600

    convertToString function.

--- src/main/kotlin/ws/billdavis/fpik/chapter03/MapAndRelated.kt
+++ src/main/kotlin/ws/billdavis/fpik/chapter03/MapAndRelated.kt
@@ -9,6 +9,14 @@ fun add1(list: MyList<Int>): MyList<Int> = when(list) {
     is Cons -> Cons(list.head+1, add1(list.tail))
 }
 
+fun convertToString(list: MyList<Double>): MyList<String> = when(list) {
+    is Nil -> Nil
+    is Cons -> Cons(list.head.toString(), convertToString(list.tail))
+}
+
+fun <A,B> map(list: MyList<A>, f: (A) -> B): MyList<B> {
+    return Nil
+}
 
 @RunWith(KTestJUnitRunner::class)
 class MapAndRelatedTests: FeatureSpec() {
@@ -16,6 +24,12 @@ class MapAndRelatedTests: FeatureSpec() {
         feature("add1()") {
             scenario("add 1 to all items") {
                 add1(myListOf(1,2,3,4)) shouldBe myListOf(2,3,4,5)
+            }
+        }
+        feature("convertToString()") {
+            scenario("convert all items to Strings") {
+                convertToString(myListOf(1.1,2.2,3.3,4.4)) shouldBe
+                    myListOf("1.1", "2.2", "3.3", "4.4")
             }
         }
     }
