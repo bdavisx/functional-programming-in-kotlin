dc655ae8348f68ba4649eb4c897844d9d1a28a85
bdavisx <bdavisx@yahoo.com>
2016-11-28 20:50:19 -0600

    ZipWith.

--- src/main/kotlin/ws/billdavis/fpik/chapter03/MapAndRelated.kt
+++ src/main/kotlin/ws/billdavis/fpik/chapter03/MapAndRelated.kt
@@ -39,32 +39,37 @@ fun <A> filterViaFlatMap(list: MyList<A>, f: (A) -> Boolean): MyList<A> =
 
 fun addLists(list1: MyList<Int>, list2: MyList<Int>): MyList<Int> =
     when(list1) {
-        is Nil -> when(list2) {
-            is Nil -> Nil
-            is Cons -> list2
-        }
+        is Nil -> Nil
         is Cons -> when(list2) {
-            is Nil -> list1
+            is Nil -> Nil
             is Cons -> Cons(list1.head + list2.head, addLists(list1.tail, list2.tail))
         }
     }
 
+fun <A,B,C> zipWith(list1: MyList<A>, list2: MyList<B>, f: (A,B) -> C ): MyList<C> =
+    when(list1) {
+        is Nil -> Nil
+        is Cons -> when(list2) {
+            is Nil -> Nil
+            is Cons -> Cons(f(list1.head, list2.head), zipWith(list1.tail, list2.tail, f))
+        }
+    }
 
 @RunWith(KTestJUnitRunner::class)
 class MapAndRelatedTests: FeatureSpec() {
     init {
-        feature("add1()") {
+        feature("add1") {
             scenario("add 1 to all items") {
                 add1(myListOf(1,2,3,4)) shouldBe myListOf(2,3,4,5)
             }
         }
-        feature("convertToString()") {
+        feature("convertToString") {
             scenario("convert all items to Strings") {
                 convertToString(myListOf(1.1,2.2,3.3,4.4)) shouldBe
                     myListOf("1.1", "2.2", "3.3", "4.4")
             }
         }
-        feature("map()") {
+        feature("map") {
             scenario("add 1 to all items") {
                 map(myListOf(1,2,3,4), { a -> a + 1 }) shouldBe myListOf(2,3,4,5)
             }
@@ -73,13 +78,13 @@ class MapAndRelatedTests: FeatureSpec() {
                     myListOf("1.1", "2.2", "3.3", "4.4")
             }
         }
-        feature("flatMap()") {
+        feature("flatMap") {
             // from exercise 3.20
             scenario("flatMap(List(1,2,3))(i => List(i,i)) == List(1,1,2,2,3,3)") {
                 flatMap(myListOf(1,2,3), { i -> myListOf(i,i) }) shouldBe myListOf(1,1,2,2,3,3)
             }
         }
-        feature("filter()") {
+        feature("filter") {
             scenario("handle nil") {
                 filter(Nil as MyList<Int>, { a -> a % 2 == 0 }) shouldBe Nil
             }
@@ -88,7 +93,7 @@ class MapAndRelatedTests: FeatureSpec() {
                     myListOf(2, 4, 6, 8)
             }
         }
-        feature("filterViaFlatMap()") {
+        feature("filterViaFlatMap") {
             scenario("handle nil") {
                 filterViaFlatMap(Nil as MyList<Int>, { a -> a % 2 == 0 }) shouldBe Nil
             }
@@ -97,18 +102,27 @@ class MapAndRelatedTests: FeatureSpec() {
                     myListOf(2, 4, 6, 8)
             }
         }
-        feature("addLists()") {
+        feature("addLists") {
             scenario("nil on left") {
-                addLists(Nil, myListOf(4, 5, 6)) shouldBe myListOf(4, 5, 6)
+                addLists(Nil, myListOf(4, 5, 6)) shouldBe Nil
             }
             scenario("nil on right") {
-                addLists(myListOf(1, 2, 3), Nil) shouldBe myListOf(1, 2, 3)
-            }
-            scenario("different length lists") {
-                addLists(myListOf(1, 2, 3, 4), myListOf(4, 5, 6)) shouldBe myListOf(5, 7, 9, 4)
+                addLists(myListOf(1, 2, 3), Nil) shouldBe Nil
             }
             scenario("exercise 3.22") {
                 addLists(myListOf(1, 2, 3), myListOf(4, 5, 6)) shouldBe myListOf(5, 7, 9)
+            }
+        }
+        feature("zipWith") {
+            scenario("nil on left") {
+                zipWith(Nil, myListOf(4, 5, 6), {a, b -> 0}) shouldBe Nil
+            }
+            scenario("nil on right") {
+                zipWith(myListOf(1, 2, 3), Nil, {a, b -> 0}) shouldBe Nil
+            }
+            scenario("exercise 3.22") {
+                zipWith(myListOf(1, 2, 3), myListOf("a", "b", "C"), {n, l -> "$n $l"}) shouldBe
+                    myListOf("1 a", "2 b", "3 C")
             }
         }
     }
