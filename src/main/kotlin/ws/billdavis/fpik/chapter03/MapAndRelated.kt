23479332e2f9063a02991bbac8505e0806d659a9
bdavisx <bdavisx@yahoo.com>
2016-11-28 19:35:45 -0600

    flatMap.

--- src/main/kotlin/ws/billdavis/fpik/chapter03/MapAndRelated.kt
+++ src/main/kotlin/ws/billdavis/fpik/chapter03/MapAndRelated.kt
@@ -19,6 +19,19 @@ fun <A,B> map(list: MyList<A>, f: (A) -> B): MyList<B> = when(list) {
     is Cons -> Cons(f(list.head), map(list.tail, f))
 }
 
+
+fun <A,B> flatMap(list: MyList<A>, f: (A) -> MyList<B>): MyList<B> = when(list) {
+    is Nil -> Nil
+    is Cons -> concatenate<B>(myListOf(f(list.head), flatMap(list.tail, f)))
+}
+
+fun <A> filter(list: MyList<A>, f: (A) -> Boolean): MyList<A> = when(list) {
+    is Nil -> Nil
+    is Cons ->
+        if(f(list.head)) { Cons(list.head, filter(list.tail, f)) }
+        else { filter(list.tail, f) }
+}
+
 @RunWith(KTestJUnitRunner::class)
 class MapAndRelatedTests: FeatureSpec() {
     init {
@@ -40,6 +53,21 @@ class MapAndRelatedTests: FeatureSpec() {
             scenario("convert all items to Strings") {
                 map(myListOf(1.1,2.2,3.3,4.4), { a -> a.toString() }) shouldBe
                     myListOf("1.1", "2.2", "3.3", "4.4")
+            }
+        }
+        feature("flatMap()") {
+            // from exercise 3.20
+            scenario("flatMap(List(1,2,3))(i => List(i,i)) == List(1,1,2,2,3,3)") {
+                flatMap(myListOf(1,2,3), { i -> myListOf(i,i) }) shouldBe myListOf(1,1,2,2,3,3)
+            }
+        }
+        feature("filter()") {
+            scenario("handle nil") {
+                filter(Nil as MyList<Int>, { a -> a % 2 == 0 }) shouldBe Nil
+            }
+            scenario("filter out odd numbers") {
+                filter(myListOf(1, 2, 3, 4, 5, 6, 7, 8, 9), { a -> a % 2 == 0 }) shouldBe
+                    myListOf(2, 4, 6, 8)
             }
         }
     }
