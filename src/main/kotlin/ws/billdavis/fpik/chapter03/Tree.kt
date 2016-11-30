7b9365e1b85dc799a24b548f503452037f26c0e6
bdavisx <bdavisx@yahoo.com>
2016-11-29 20:12:05 -0600

    maximum(tree)

--- src/main/kotlin/ws/billdavis/fpik/chapter03/Tree.kt
+++ src/main/kotlin/ws/billdavis/fpik/chapter03/Tree.kt
@@ -14,15 +14,41 @@ sealed class Tree<out A> {
     }
 }
 
+tailrec fun maximum(tree: Tree<Int>): Int {
+    fun loop(tree: Tree<Int>, currentMaximum: Int): Int {
+        return when (tree) {
+            is Tree.Leaf<Int> -> Math.max(tree.value, currentMaximum)
+            is Tree.Branch<Int> -> loop(tree.left,loop(tree.right, currentMaximum))
+        }
+    }
+    return loop(tree, Int.MIN_VALUE)
+}
+
 @RunWith(KTestJUnitRunner::class)
 class TreeTests: FeatureSpec() {
     init {
-        feature("Tree") {
-            scenario("single leaf s/b 1") {
+        feature("maximum(tree)") {
+            scenario("max of single leaf s/b value iof that leaf") {
+                val tree: Tree<Int> = Tree.Leaf(10)
+                maximum(tree) shouldBe 10
+            }
+            scenario("maximum of multiple branches/trees s/b correct") {
+                val tree: Tree<Int> =
+                    Tree.Branch(
+                        Tree.Leaf(1),
+                        Tree.Branch(
+                            Tree.Leaf(2),
+                            Tree.Leaf(3)))
+
+                maximum(tree) shouldBe 3
+            }
+        }
+        feature("Tree.size") {
+            scenario("size of single leaf s/b 1") {
                 val tree: Tree<Int> = Tree.Leaf(1)
                 tree.size() shouldBe 1
             }
-            scenario("multiple branches/trees s/b correct") {
+            scenario("size of multiple branches/trees s/b correct") {
                 val tree: Tree<Int> =
                     Tree.Branch(
                         Tree.Leaf(1),
